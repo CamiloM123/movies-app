2ada6abebc877d1df306ecf258526719fd9ec8a7
CamiloM <juanc.munozv@autonoma.edu.co>
2023-12-05 02:39:39 -0500

    Create frontend auth page, login and register.

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,7 +1,100 @@
+"use client";
+
+import { useCallback, useState } from "react"
+import Input from "@/components/Input"
+import Image from "next/image"
+
+
 const Auth = () => {
+    const [name, setName] = useState('')
+    const [lastname, setLastname] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [variant, setVariant] = useState<'login' | 'register' >('login')
+
+    const toggleVariant = useCallback(() => {
+        setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
+    }, [])
+
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeatbg-center bg-fixed bg-cover">
-            
+            <div className="bg-black w-full h-full lg:bg-opacity-50">
+                <nav className="px-10 py-3">
+                    <Image src="/images/logo.png" alt="Logo" width={150} height={48}/>
+                </nav>
+                {/* Container */}
+                <div className="flex justify-center">
+                    <div className="bg-black bg-opacity-70 px-12 py-7 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+                        <h2 className="text-white text-4xl mb-8 font-semibold ">
+                            {variant === 'login' ? 'Sign in' : 'Register'}
+                        </h2>
+                        <div className="flex flex-col gap-4">
+                            {/* Register */}
+                            {variant === 'register' && (
+                                <div className="flex gap-4">
+                                    <Input
+                                        label="Name"
+                                        onChange={(ev: any) => setName(ev.target.value)}
+                                        id="name"
+                                        type="text"
+                                        value={name}
+                                    />
+                                    <Input
+                                        label="Lastname"
+                                        onChange={(ev: any) => setLastname(ev.target.value)}
+                                        id="lastname"
+                                        type="text"
+                                        value={lastname}
+                                    />
+                                </div>
+                            )}
+                            {/* Login */}
+                            <Input
+                                label="Email"
+                                onChange={(ev: any) => setEmail(ev.target.value)}
+                                id="email"
+                                type="email"
+                                value={email}
+                            />
+                            <Input
+                                label="Password"
+                                onChange={(ev: any) => setPassword(ev.target.value)}
+                                id="password"
+                                type="password"
+                                value={password}
+                            />
+
+                            {/* Terminos y condiciones */}
+                            {variant === 'register' && (
+                                <div className="flex items-center gap-1">
+                                    <input
+                                        type="checkbox"
+                                        name="terms"
+                                        id="terms"
+                                        onChange={() => {}}
+                                        className="hidden"
+                                    />
+                                    <label htmlFor="terms" className="text-white cursor-pointer hover:underline">
+                                        I agree to the terms and conditions
+                                    </label>
+                                </div>
+                            )}
+                            
+
+
+                        </div>
+                        <button className="bg-red-600 py-3 rounded-md text-white w-full mt-10 hover:bg-red-700 transition">
+                            {variant === 'login' ? 'Login' : 'Sign up'} 
+                        </button>
+                        <p className="text-neutral-500 mt-8">
+                            {variant === 'login' ? 'Dont have an account?' : 'Already have an account?'}
+                            <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
+                                {variant === 'login' ? 'Create an account' : 'Login'}
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
